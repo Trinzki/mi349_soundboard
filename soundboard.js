dd7c0208876d5e238a02fe49c7b27f03e72057ab
Trinzki <cmtrocinski@gmail.com>
2021-11-15 13:33:25 -0500

    aduio

--- soundboard.js
+++ soundboard.js
@@ -1,12 +1,12 @@
 
-function play(){
+function playAudio(){
     alert("Got here")
     // var audio = document.getElementsByClassName("audio");
     // audio.play();
 }
 
 
-document.getElementById('boing').addEventListener("click", play());
+document.getElementById('boing').addEventListener("click", playAudio());
 document.getElementById('bubbling').addEventListener("click", play());
 let cymbalBtn = document.getElementById('cymbals');
 cymbalBtn.addEventListener("click", play());
