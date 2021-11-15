1d9832f458b74232adeafc34a21d72ceaff0263e
Trinzki <cmtrocinski@gmail.com>
2021-11-15 13:47:32 -0500

    audio function

--- soundboard.js
+++ soundboard.js
@@ -1,7 +1,21 @@
 
-function playAudio(){
-    var audio = document.getElementsByClassName("audio");
-    audio.play();
+function playAudio(input){
+    let id;
+    if (input === 1){
+        id = "boing_sound";
+    }
+    else if(input === 2){
+        id = "bubble_sound";
+    }
+    else if(input === 3){
+        id = "cymbals_sound";
+    }
+    else{
+        alert("There was a sound issue");
+        return;
+    }
+
+    document.getElementById(id).play();
 }
 
 
