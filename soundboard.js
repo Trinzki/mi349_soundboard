ae14e680b49148e34f378a0291594ba82aac3011
Trinzki <cmtrocinski@gmail.com>
2021-11-18 20:07:22 -0500

    using jeff's code for js

--- soundboard.js
+++ soundboard.js
@@ -1,27 +1,9 @@
 
 function playAudio(){
-    let id;
-    if (this === "boing"){
-        id = "boing_sound";
-    }
-    else if(this === "bubbling"){
-        id = "bubble_sound";
-    }
-    else if(this === "cymbals"){
-        id = "cymbals_sound";
-    }
-    else{
-        alert("There was a sound issue");
-        return;
-    }
-
-    var sound = document.getElementById(id)
-    alert(id);  // For Debugging and making sure the function 
-                // is called with the right id
+    var sound = document.getElementById(this.childNodes[1].id);
     sound.play();
 }
-
-
+  
 document.getElementById("boing").addEventListener("click", playAudio);
 document.getElementById('bubbling').addEventListener("click", playAudio);
 document.getElementById('cymbals').addEventListener("click", playAudio);
